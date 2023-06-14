d0a22ca7267c0d5d37b6e51bb0b398aa1f59e49e
enjimenez <enjimenez@live.com.mx>
2023-06-14 17:24:47 -0600

    Build for EC2 Server

---
+++ build/precache-manifest.bec2990d390b9eb19d4629a9065d2171.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94d58d4aef515f53f9d6ac51ab54973",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d01aa3f536e87a9b4b2f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e50a6b2b155eda61ffd9",
+    "url": "/static/css/main.7071ae30.chunk.css"
+  },
+  {
+    "revision": "d01aa3f536e87a9b4b2f",
+    "url": "/static/js/2.67710b93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.67710b93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e50a6b2b155eda61ffd9",
+    "url": "/static/js/main.9e8fb848.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -2,7 +2,7 @@
 export async function getAllUsers() {
 
     try{
-        const response = await fetch('/note/all', {
+        const response = await fetch('http://44.212.157.81/api/note/all', {
             method: 'GET',
             headers: {'ngrok-skip-browser-warning': '69420'},
         });
@@ -14,7 +14,7 @@ export async function getAllUsers() {
 }
 
 export async function createUser(data) {
-    const response = await fetch(`/note`, {
+    const response = await fetch(`http://44.212.157.81/api/note`, {
         method: 'POST',
         headers: {'Content-Type': 'application/json', 'ngrok-skip-browser-warning': '69420'},
         body: JSON.stringify({title:data.title, content:data.content})
